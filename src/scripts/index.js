295cb45ef98d06e472ec6af9a41a19dad75d8d96
Tyshchenko Oleksandr <o.tyshchenko19.03.1994@gmail.com>
2021-06-10 14:25:37 +0300

    format code

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,29 +1,33 @@
-import {graph} from './graph';
+import { graph } from './graph';
 
 const getMiddleValue = (obj) => obj.children.reduce((acc, el) => {
     if (el.children) {
-    const i =  getMiddleValue(el);
-    return {
-        sum: acc.sum + i.sum,
-        counter: acc.counter + i.counter
-    }
-    } else return {
-            sum: acc.sum + el.value,
-            counter: acc.counter + 1
+        const { sum, counter } = getMiddleValue(el);
+        return {
+            sum: acc.sum + sum,
+            counter: acc.counter + counter
         }
-},{sum: obj.value, counter: 1});
+    } else {
+        return {
+            sum: acc.sum + el.value,
+            counter: ++acc.counter
+        }
+    }
+}, { sum: obj.value, counter: 1 });
 
 const result = getMiddleValue(graph);
 console.log(result.sum / result.counter);
 
 const getMin = (a, b) => a.value < b.value ? a : b;
 const getMinValue = (obj) =>
-    obj.children.reduce((max, el) => (el.children) ? getMin(getMinValue(el), max) : getMin(el, max), obj);
+    obj.children.reduce((max, el) =>
+        (el.children) ? getMin(getMinValue(el), max) : getMin(el, max), obj);
 
 console.log(getMinValue(graph));
 
 const getMax = (a, b) => a.value > b.value ? a : b;
 const getMaxValue = (obj) =>
-    obj.children.reduce((max, el) => (el.children) ? getMax(getMaxValue(el), max) : getMax(el, max), obj); 
+    obj.children.reduce((max, el) =>
+        (el.children) ? getMax(getMaxValue(el), max) : getMax(el, max), obj);
 
 console.log(getMaxValue(graph));
